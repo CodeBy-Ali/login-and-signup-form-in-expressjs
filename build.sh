111899e2ba830fda0d0a52fb9b2ff21b137b0255
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-11-02 14:48:25 +0500

    build: add production false flag in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # install dependencies
 npm install --prefix client --production=false
-npm install --prefix server 
+npm install --prefix server --production=false
 
 # build both projects
 npm run build
