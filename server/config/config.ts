15140b2f33849674c6888a0377770a855a99cf17
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-11-02 14:03:44 +0500

    feat: add config interface

--- server/config/config.ts
+++ server/config/config.ts
@@ -6,18 +6,46 @@ const __dirname = import.meta.dirname;
 const envPath = path.resolve(__dirname, '../.env')
 dotenv.config({ path: envPath });
 
-const config = {
-  PORT: Number(process.env.PORT) || 3000,
-  HOST: process.env.HOST || `127.0.0.1`,
+type Env = "testing" |"development" | "production"
+
+interface Config{
+  port: number,
+  host: string,
+  dir: {
+    root: string,
+    static: string,
+    views: string,
+  },
+  Client: {
+    id: string,
+    secret: string,
+    redirectUri: string,
+    scopes: string[]
+  },
+  database: string,
+  bcrypt: {
+    saltRounds: number
+  },
+  sessionSecret: string,
+  cookie: {
+    name: string,
+    maxAge: number
+  }
+  env: Env
+}
+
+const config:Config = {
+  port: Number(process.env.PORT) || 3000,
+  host: process.env.HOST || `127.0.0.1`,
   dir: {
     root: __dirname,
     static: path.resolve(__dirname, '..', '../client/public') + sep,
     views: path.resolve(__dirname, '../views') + sep,
   },
   Client: {
-    id: process.env.CLIENT_ID,
-    secret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI,
+    id: process.env.CLIENT_ID as string,
+    secret: process.env.CLIENT_SECRET as string,
+    redirectUri: process.env.REDIRECT_URI as string,
     scopes:  [
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
@@ -31,8 +59,8 @@ const config = {
   cookie: {
     name: "sessionId",
     maxAge: 5 * 60 * 1000, // 5 minutes
-  }
-  
+  },
+  env: process.env.NODE_ENV as Env || "development"
 }
 
 export default config;
